ec117c097a4f3eefc1af9dffb3c91f51c75d32a1
yelvertoncory <31113087+yelvertoncory@users.noreply.github.com>
2017-12-07 12:19:16 -0500

    Add files via upload

--- code/Cart.java
+++ code/Cart.java
@@ -15,49 +15,66 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Cart implements Iterator{
+    //keeps a running total of the cost of items in the cart
     private double total;
+    //counts the number of items in the cart
     private int count;
+    //using an  ArrayList for my cart
     private ArrayList<Object> cart = new ArrayList<>();
     
-    public Cart(double total, int count){
+    //constructor for cart
+    public Cart(double total, int count, ArrayList<Object> cart){
         this.total = total;
         this.count = count;
-        ArrayList<Object> cart = new ArrayList<Object>();
+        this.cart = new ArrayList<Object>();
     }
     
+    //adds the cost to the total. will pass a shoe.getCost() eventually
     public void addToTotal(double cost){
         total+=cost;
     }
+    // adds one to the count everytime a shoe is added
     public void addToCount(){
         count++;
     }
+    
+    //Adds an object based on the object name
     public void addProduct(Object o){
         cart.add(o);
         addToCount();
         //addToTotal(o.getCost);
     }
-    
+    //adds an object by letting you specify the index
+    public void addProduct(int index, Object o){
+        cart.add(index,o);
+         //addToTotal(o.getCost);
+    }
+    //returns the value of total when called
     public double getTotal(Cart c){
         return c.total;
     }
+    //returns the current number of items in cart when called
     public int getCount(Cart c){
         return c.count;
     }
     
+    //removes a specified object when called
     public void deleteItem(Object o){
         if(!hasNext()){
             cart.remove(o);
+            //total = total - o.getCost();
+            count -= 1;
         }
     }
-    
+    //clears out the current cart and sets total/count to 0
     public void clearCart(){
         cart.clear();
+        count = 0;
+        total = 0;
     }
     
     public void checkout(Cart c){
-       c.cart.clear();
-       this.count  = 0;
-       this.total = 0;
+      //not sure yet
     }
 
     @Override
